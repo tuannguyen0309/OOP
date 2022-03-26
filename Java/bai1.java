eda02899e9759bf5296151c40c70058b1abd3305
tuannguyen0309 <tuannguyenviet0309@gmail.com>
2022-03-27 00:45:24 +0700

    baitap

--- Java/bai1.java
+++ Java/bai1.java
@@ -1,50 +1,50 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class bai1 {
+// public class bai1 {
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
 
-        int a[] = new int[10];
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Nhap 10 phan tu cua mang: ");
-            for (int i = 0; i < 10; i++) {
-                a[i] = sc.nextInt();
-            }
-        }
-        System.out.println("Mang vua nhap: ");
-        for (int i = 0; i < 10; i++) {
-            System.out.print("\t" + a[i]);
-        }
-        System.out.println("\n");
-        int temp;
-         for (int i = 0 ; i < 9; i++) {
-            for (int j = i + 1; j < 10; j++) {
-                if (a[i] > a[j]) {
-                    temp = a[j];
-                    a[j] = a[i];
-                    a[i] = temp;
-                }
-            }
-        }
-         System.out.println("Mang Tang Dan La: ");
-        for (int i = 0; i < 10; i++) {
-            System.out.print("\t" + a[i]);
-        }
-         System.out.println("\n");
-         for (int i = 0 ; i < 9; i++) {
-            for (int j = i + 1; j < 10; j++) {
-                if (a[i] < a[j]) {
-                    temp = a[j];
-                    a[j] = a[i];
-                    a[i] = temp;
-                }
-            }
-        }
-          System.out.println("Mang Giam Dan La: ");
-        for (int i = 0; i < 10; i++) {
-            System.out.print("\t" + a[i]);
-        }
-         System.out.println("\n");
-    }
+//         int a[] = new int[10];
+//         try (Scanner sc = new Scanner(System.in)) {
+//             System.out.println("Nhap 10 phan tu cua mang: ");
+//             for (int i = 0; i < 10; i++) {
+//                 a[i] = sc.nextInt();
+//             }
+//         }
+//         System.out.println("Mang vua nhap: ");
+//         for (int i = 0; i < 10; i++) {
+//             System.out.print("\t" + a[i]);
+//         }
+//         System.out.println("\n");
+//         int temp;
+//          for (int i = 0 ; i < 9; i++) {
+//             for (int j = i + 1; j < 10; j++) {
+//                 if (a[i] > a[j]) {
+//                     temp = a[j];
+//                     a[j] = a[i];
+//                     a[i] = temp;
+//                 }
+//             }
+//         }
+//          System.out.println("Mang Tang Dan La: ");
+//         for (int i = 0; i < 10; i++) {
+//             System.out.print("\t" + a[i]);
+//         }
+//          System.out.println("\n");
+//          for (int i = 0 ; i < 9; i++) {
+//             for (int j = i + 1; j < 10; j++) {
+//                 if (a[i] < a[j]) {
+//                     temp = a[j];
+//                     a[j] = a[i];
+//                     a[i] = temp;
+//                 }
+//             }
+//         }
+//           System.out.println("Mang Giam Dan La: ");
+//         for (int i = 0; i < 10; i++) {
+//             System.out.print("\t" + a[i]);
+//         }
+//          System.out.println("\n");
+//     }
 
-}
+// }
--- Java/bai1tuan2.java
+++ Java/bai1tuan2.java
@@ -1,17 +1,17 @@
-public class bai1tuan2 {
-    public static void main(String[] args){
-        int f1=1;
-        int f2=1;
-        int fn=0;
-        int sum=0;
+// public class bai1tuan2 {
+//     public static void main(String[] args){
+//         int f1=1;
+//         int f2=1;
+//         int fn=0;
+//         int sum=0;
 
-        while (fn<100) {
-            //System.out.println(f2);
-            sum += fn;
-            fn = f1 + f2;
-            f1 = f2;
-            f2 = fn;
-        }
-        System.out.println(sum);
-    }
-}
+//         while (fn<100) {
+//             //System.out.println(f2);
+//             sum += fn;
+//             fn = f1 + f2;
+//             f1 = f2;
+//             f2 = fn;
+//         }
+//         System.out.println(sum);
+//     }
+// }
